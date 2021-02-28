64074de215329bd5a3166440504170578af3bdb4
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2021-02-28 18:52:10 -0300

    removed useless code in the !STOCK command

--- src/Commands/User/GENERAL/STOCK/index.js
+++ src/Commands/User/GENERAL/STOCK/index.js
@@ -15,27 +15,23 @@ module.exports = (sender, client, users) => {
     sender,
     messages.REQUEST[users[sender.getSteamID64()].language]
   );
-  setTimeout(() => {
-    chatMessage(
-      client,
-      sender,
-      filterCommands(messages.STOCK[users[sender.getSteamID64()].language])
-        .join('\n')
-        .replace('{TOTALBOTSETS}', inventory.stock.totalBotSets)
-        .replace('{CSKEYSTRADABLE}', inventory.stock.csKeys.tradable)
-        .replace('{HYDRAKEYSTRADABLE}', inventory.stock.hydraKeys.tradable)
-        .replace('{TFKEYSTRADABLE}', inventory.stock.tfKeys.tradable)
-        .replace(
-          '{GEMSQUANTITYTRADABLE}',
-          inventory.stock.gemsQuantity.tradable
-        )
-        .replace('{CSKEYSNOTRADABLE}', inventory.stock.csKeys.notradable)
-        .replace('{HYDRAKEYSNOTRADABLE}', inventory.stock.hydraKeys.notradable)
-        .replace('{TFKEYSNOTRADABLE}', inventory.stock.tfKeys.notradable)
-        .replace(
-          '{GEMSQUANTITYNOTRADABLE}',
-          inventory.stock.gemsQuantity.notradable
-        )
-    );
-  }, 1000);
+
+  chatMessage(
+    client,
+    sender,
+    filterCommands(messages.STOCK[users[sender.getSteamID64()].language])
+      .join('\n')
+      .replace('{TOTALBOTSETS}', inventory.stock.totalBotSets)
+      .replace('{CSKEYSTRADABLE}', inventory.stock.csKeys.tradable)
+      .replace('{HYDRAKEYSTRADABLE}', inventory.stock.hydraKeys.tradable)
+      .replace('{TFKEYSTRADABLE}', inventory.stock.tfKeys.tradable)
+      .replace('{GEMSQUANTITYTRADABLE}', inventory.stock.gemsQuantity.tradable)
+      .replace('{CSKEYSNOTRADABLE}', inventory.stock.csKeys.notradable)
+      .replace('{HYDRAKEYSNOTRADABLE}', inventory.stock.hydraKeys.notradable)
+      .replace('{TFKEYSNOTRADABLE}', inventory.stock.tfKeys.notradable)
+      .replace(
+        '{GEMSQUANTITYNOTRADABLE}',
+        inventory.stock.gemsQuantity.notradable
+      )
+  );
 };
